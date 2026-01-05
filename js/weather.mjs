6a65ece8151cfb7e90686238d8ab787fd64e1e3d
hoki4 <svizygod@gmail.com>
2026-01-05 10:05:00 +0300

    Расширил API модуль - добавил получение прогноза погоды

--- js/weather.mjs
+++ js/weather.mjs
@@ -1,8 +1,22 @@
 import { API_KEY } from './vars.mjs';
 
 async function getWeatherByCoords({ lon, lat }, units = 'metric') {
-    const url = `https://api.openweathermap.org/data/2.5/weather?units=${units}&lat=${lat}&lon=${lon}&appid=${API_KEY}`
-    return (await fetch(url)).json();
+  const url = `https://api.openweathermap.org/data/2.5/weather?units=${units}&lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  return (await fetch(url)).json();
 }
 
-export { getWeatherByCoords };
+async function getForecastByCoords({ lon, lat }, units = 'metric') {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?units=${units}&lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  return (await fetch(url)).json();
+}
+
+// Получить всё сразу
+async function getAllWeather(coords, units = 'metric') {
+  const [weather, forecast] = await Promise.all([
+    getWeatherByCoords(coords, units),
+    getForecastByCoords(coords, units),
+  ]);
+  return { weather, forecast };
+}
+
+export { getWeatherByCoords, getForecastByCoords, getAllWeather };
